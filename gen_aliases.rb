e6baed427d790bfa66ecdf33f91f697b5b23300d
Carla Souza <contact@carlasouza.com>
2014-11-26 18:04:42 -0500

    Update gen_aliases.rb

--- gen_aliases.rb
+++ gen_aliases.rb
@@ -7,9 +7,6 @@ require 'json'
 raw = File.read('respostas.json').force_encoding "UTF-8"
 json = JSON.parse(raw.gsub("\n",''))
 
-test = []
 json.collect do |pergunta,respostas|
-        a = pergunta.split(' ').collect{|s|s[0]}.join
-	test << a
 	puts "/alias gg#{pergunta.gsub(' ','')} /say #{respostas.join';/say '}"
 end
